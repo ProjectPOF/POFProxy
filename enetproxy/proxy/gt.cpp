a88e3407c96ac0ff1857a3503e54cd997f2b24c3
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-05-21 16:31:28 +0300

    Update gt.cpp

--- enetproxy/proxy/gt.cpp
+++ enetproxy/proxy/gt.cpp
@@ -22,21 +22,3 @@ using namespace std;
     {
     g_server->send(true, "action|log\nmsg|" + text, NET_MESSAGE_GAME_MESSAGE);
     }
-
- 
-
-
-void gt::solve_captcha(std::string text) {
-  gt::send_log("i will add captcha later");
-    //Get the sum :D
-   // utils::replace(text,
-    //    "set_default_color|`o\nadd_label_with_icon|big|`wAre you Human?``|left|206|\nadd_spacer|small|\nadd_textbox|What will be the sum of the following "
-    //    "numbers|left|\nadd_textbox|",
-    //    "");
-   // utils::replace(text, "|left|\nadd_text_input|captcha_answer|Answer:||32|\nend_dialog|captcha_submit||Submit|", "");
-   // auto number1 = text.substr(0, text.find(" +"));
-   // auto number2 = text.substr(number1.length() + 3, text.length());
-   // int result = atoi(number1.c_str()) + atoi(number2.c_str());
-    //send_log("Solved captcha as `2" + std::to_string(result) + "``");
-    //g_server->send(false, "action|dialog_return\ndialog_name|captcha_submit\ncaptcha_answer|" + std::to_string(result));
-}
