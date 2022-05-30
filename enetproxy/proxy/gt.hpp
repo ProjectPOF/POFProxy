6d5fe17c560a93b2907e6b048fc3ad2006d06e5f
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-05-30 19:09:39 +0300

    Update gt.hpp

--- enetproxy/proxy/gt.hpp
+++ enetproxy/proxy/gt.hpp
@@ -11,5 +11,5 @@ namespace gt {
     extern bool in_game;
     extern bool ghost;
     void send_log(std::string text);
-    void SolveCaptcha(std::string captcha);
+    void SolveCaptcha(std::string text);
 }
