3fd1a8b00b9cd2a93c48551c6c478d2f8ad7aed1
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-06-02 19:22:24 +0300

    puzzle captcha thing bored asf

--- enetproxy/proxy/events.cpp
+++ enetproxy/proxy/events.cpp
@@ -8,7 +8,21 @@
 #include <thread>
 #include <limits.h>
 #include "HTTPRequest.hpp"
-#include "gt.cpp"
+
+std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos - prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
+}
 
 bool events::out::variantlist(gameupdatepacket_t* packet) {
     variantlist_t varlist{};
